d226a9c476f4b7c4c546dad92e1e87b9d9353eb2
Hassane Moustapha Ousmane <hmoustaphaousmane01@gmail.com>
2024-07-30 10:36:32 +0000

    feat: check user existance

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,28 @@ NUMBER_TO_GUESS=$(( (RANDOM % 1000) + 1))
 
 echo "Enter your username: "
 read USERNAME
+
+# get user id
+USER_ID=$($PSQL "select user_id from users where username = '$USERNAME';")
+
+# if user id does not exist
+if [[ -z $USER_ID ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+  # insert new user
+  INSERT_USER_RESULT=$($PSQL "insert into users (username) values ('$USERNAME');")
+
+  # get new user id
+  USER_ID=$($PSQL "select user_id from users where username = '$USERNAME';")
+else
+  # get database username
+  DB_USERNAME=$($PSQL "select username from users where user_id = $USER_ID;")
+
+  # get user's games info
+  GAME=$($PSQL "select count(*), min(guesses) from games where user_id = $USER_ID;")
+  echo "$GAME" | while IFS='|' read GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $DB_USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
